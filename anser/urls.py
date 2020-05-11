9b9003a06265b8ef4f7cf4a9f3565d0667a222b0
Michał Górny <mgorny@gentoo.org>
2020-05-11 08:27:34 +0200

    gstats → anser
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- anser/urls.py
+++ anser/urls.py
@@ -1,4 +1,4 @@
-"""gstats URL Configuration
+"""anser URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- anser/wsgi.py
+++ anser/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gstats project.
+WSGI config for anser project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gstats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anser.settings')
 
 application = get_wsgi_application()
